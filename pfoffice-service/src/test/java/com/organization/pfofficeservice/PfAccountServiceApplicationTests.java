314b7e3d915fb7f1e92bc321f76cbbe78c371f9b
Rajasekhar <rajasekharp7java@gmail.com>
2025-07-17 07:35:27 +0530

    pfoffice service changes with mongodb and dto classes to send request and response as per our requirement for pom xml file and application properties and unversioned files

---
+++ pfoffice-service/src/test/java/com/organization/pfofficeservice/PfAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.organization.pfofficeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
