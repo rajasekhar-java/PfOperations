2028a9504a9108283cf2e89c7c9e5dd8ab52207d
Rajasekhar <rajasekharp7java@gmail.com>
2025-07-25 08:51:31 +0530

    provide security for pfoffice service and send request through api gateway

---
+++ apigateway/src/main/java/com/organization/apigateway/configure/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.organization.apigateway.configure;
+
+import com.organization.apigateway.KeycloakRoleConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/public/**").permitAll()
+                        .pathMatchers("/api/**").authenticated()
+                        .anyExchange().denyAll()
+                )
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt)
+                .build();
+    }
+}
+
+
--- pfoffice-service/src/main/java/com/organization/pfofficeservice/controller/PfAccountController.java
+++ pfoffice-service/src/main/java/com/organization/pfofficeservice/controller/PfAccountController.java
@@ -2,10 +2,15 @@ package com.organization.pfofficeservice.controller;
 import com.organization.pfofficeservice.dto.PfAccountRequest;
 import com.organization.pfofficeservice.dto.PfAccountResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.*;
 import com.organization.pfofficeservice.service.PfAccountService;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import com.organization.pfofficeservice.entity.PfAccount;
 
 import java.util.List;
@@ -22,33 +27,47 @@ public class PfAccountController {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<PfAccountResponse> createPFAccount(@RequestBody PfAccountRequest request) {
         PfAccountResponse response = pfAccountService.createPFAccount(request);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<PfAccountResponse> getPFAccountById(@PathVariable String id) {
         PfAccountResponse response = pfAccountService.getPFAccountById(id);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/by-pfid/{pfid}")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<PfAccountResponse> getPFAccountByPfid(@PathVariable String pfid) {
         PfAccountResponse response = pfAccountService.getPFAccountByPfid(pfid);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/by-employeeid/{employeeId}")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<PfAccountResponse> getPFAccountByEmployeeId(@PathVariable String employeeId) {
         PfAccountResponse response = pfAccountService.getPFAccountByEmployeeId(employeeId);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<List<PfAccountResponse>> getAllPFAccounts() {
         List<PfAccountResponse> response = pfAccountService.getAllPFAccounts();
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/public/info")
+    public String publicInfo() {
+        return "Public info";
+    }
+    @GetMapping("/me")
+    public String getUserInfo(@AuthenticationPrincipal Jwt jwt) {
+        return "Hello, " + jwt.getClaims();
     }
 
 }
